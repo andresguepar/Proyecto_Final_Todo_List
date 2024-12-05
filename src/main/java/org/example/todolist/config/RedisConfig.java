b2abf9b27e9bbbe1ec0eecc0deb92e0320dd17a1
andresguepar <34109816+andresguepar@users.noreply.github.com>
2024-12-05 16:42:04 -0500

    Redis

---
+++ src/main/java/org/example/todolist/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package org.example.todolist.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory("redis", 6379);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
+    }
+}
